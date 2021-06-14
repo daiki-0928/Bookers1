aae491d99747c520331dd1f3f4612dbd497f48a1
daiki-0928 <d.brown28112545@gmail.com>
2021-06-14 12:26:03 +0000

    [add]ルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,5 @@
 Rails.application.routes.draw do
-  get 'homes/index'
-  get 'homes/show'
-  get 'homes/new'
-  get 'homes/edit'
-  get 'homess/index'
-  get 'homess/show'
-  get 'homess/new'
-  get 'homess/edit'
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/edit'
-  get 'book/index'
-  get 'book/show'
-  get 'book/new'
-  get 'book/edit'
+  root :to =>'homes#top'
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
