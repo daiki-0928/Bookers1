5c02c12d49dd8f6f1d4bee87904dd23d7a5ee4c3
daiki-0928 <d.brown28112545@gmail.com>
2021-06-14 10:58:01 +0000

    [add]viewファイル

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  get 'homes/index'
+  get 'homes/show'
+  get 'homes/new'
+  get 'homes/edit'
+  get 'homess/index'
+  get 'homess/show'
+  get 'homess/new'
+  get 'homess/edit'
+  get 'books/index'
+  get 'books/show'
+  get 'books/new'
+  get 'books/edit'
+  get 'book/index'
+  get 'book/show'
+  get 'book/new'
+  get 'book/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
