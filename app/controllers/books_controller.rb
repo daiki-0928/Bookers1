cac92ebf08e984925285b86cf613c7614081cc9f
daiki-0928 <d.brown28112545@gmail.com>
2021-06-15 04:32:17 +0000

    [add]bookの新規登録機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,24 @@ class BooksController < ApplicationController
   end
 
   def show
+   @list = List.find(params[:books])
   end
 
   def new
+   @blog = Book.new
+  end
+
+  def create
+    blog = Book.new(book_params)
+    blog.save
+    redirect_to boos_path
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
