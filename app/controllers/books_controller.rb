da62f713427cd1f2380ede1af7d568beb9d0d08e
daiki-0928 <d.brown28112545@gmail.com>
2021-06-16 04:55:33 +0000

    [add]フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,20 +2,30 @@ class BooksController < ApplicationController
 protect_from_forgery
   def index
    @books = Book.all
+   @book = Book.new
   end
 
   def show
-   @book = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books'
+    @book = Book.new(book_params)
+   if @book.save
+      redirect_to book_path(@book.id) , notice: 'Book was successfully created.'
+   else
+    flash.now[:alert] = '2 errors prohibited this book from being saved:'
+   end
   end
 
   def edit
-   @book = Book.find(params[:id])
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
   end
 
   def destroy
